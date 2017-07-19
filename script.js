ddb0133d06544a31e91d46e81d3bd63915e3900f
Emily Oliver <mleeoliver@gmail.com>
2017-07-19 23:08:39 +0100

    Make gaps even, make sure lines don't intersect

--- script.js
+++ script.js
@@ -46,24 +46,62 @@ function genConstellation() {
 	var cx = 250; //Central point on axis x
 	var cy = 250; //central point on axis y
 	//randomise the starting position
-	var x = cx + tombola.range(-50, 50); //pick the place to start drawing the constellation 
-	var y = cy + tombola.range(-50, 50);
+	var x = cx; //pick the place to start drawing the constellation 
+	var y = cy;
 	var min = 2; //min distance between stars
 	var max = 50; //max dist. between stars
 	var prevPosition = new Point(x, y);
+	var prevSize;
 
 	//Start at prevPosition and then generate a star at a new position based on prevPosition + a new random x and y value
 	for(var i=0;i<starCount;i++) {
 		var randomx = tombola.rangeFloat(min, max);
 		if (tombola.percent(50)) randomx = -randomx;
 		var randomy = tombola.rangeFloat(min, max);
-		if (tombola.percent(50)) randomy = -randomy;		
+		if (tombola.percent(50)) randomy = -randomy;
+		//This makes first star origin of constellation (cancelling randomisation for the origin)
+		if (i === 0) { 
+			randomx = 0;
+			randomy = 0;
+		}		
 		var position = new Point(prevPosition.x + randomx, prevPosition.y + randomy);
-		var size = tombola.rangeFloat(0.5, 2); ///////make it so there's less of a chance of the bigger size stars appearing///////
+
+		//Check for intersection
+		do {
+			//Set intersection to false (if during for loop intersection doesn't return true it will stay false and therefore draw the line(store the line points))
+			var intersection = false;
+			//Check each line for intersection and if true set intersection back to true and return to top of while loop and run again
+			for(var j = 0; j < myConstellation.lines.length; j++) {
+				var l = myConstellation.lines[j];
+				if (lineIntersect(position.x, position.y, prevPosition.x, prevPosition.y, l.start.x, l.start.y, l.end.x, l.end.y)) {
+					//Intersection has occurred so this fails the test so it goes back to top of while loop
+					intersection = true;
+					console.log("An intersection has occurred");
+					//break;
+				}
+			}
+			if (intersection) {
+				randomx = tombola.rangeFloat(min, max);
+				if (tombola.percent(50)) randomx = -randomx;
+				randomy = tombola.rangeFloat(min, max);
+				if (tombola.percent(50)) randomy = -randomy;
+				position = new Point(prevPosition.x + randomx, prevPosition.y + randomy);
+			}
+		}
+		while (intersection);
+
+		var size = tombola.rangeFloat(0.5, 5); ///////make it so there's less of a chance of the bigger size stars appearing///////
+		
+		// CREATE STAR//
 		var star = new Star(position, size);
 		myConstellation.stars.push(star);
-		var line = new Line(prevPosition.clone(), position.clone());
+
+		//CREATELINE //
+		if (prevSize === undefined) prevSize === size;
+		var line = new Line(prevPosition.clone(), position.clone(), prevSize, size);
+
 		myConstellation.lines.push(line);
+		prevSize = size; //remembers this star's size
 		prevPosition = position; //So that each new star is positioned relative to the last
 	}
 
@@ -96,9 +134,11 @@ Point.prototype.clone = function() {
 	return new Point(this.x, this.y);
 }
 
-function Line(start, end) {
-	this.start = start;
+function Line(start, end, startRadius, endRadius) {
+	this.start = start;  // is a point object
 	this.end = end;
+	this.startRadius = startRadius;
+	this.endRadius = endRadius;
 	this.calcGaps();
 }
 
@@ -112,16 +152,18 @@ Line.prototype.draw = function() {
 };
 
 Line.prototype.calcGaps = function() {
-	var vector = new Vector(this.end.x - this.start.x, this.end.y - this.start.y);
-	console.log(vector);
-	vector.normalise();
-	console.log(vector);
-	vector.multiply(5); //How can I reference the size of that particular star from within here?
-	console.log(vector);
-	this.start.x += vector.x;
-	this.start.y += vector.y;
-	this.end.x -= vector.x;
-	this.end.y -= vector.y;
+	var startVector = new Vector(this.end.x - this.start.x, this.end.y - this.start.y); //does this need to change?
+	console.log(startVector);
+	startVector.normalise();
+	console.log(startVector);
+	startVector.multiply(4 + this.startRadius); //How can I reference the size of that particular star from within here?
+	var endVector = startVector.clone();
+	endVector.normalise();
+	endVector.multiply(4 + this.endRadius);
+	this.start.x += startVector.x;
+	this.start.y += startVector.y;
+	this.end.x -= endVector.x;
+	this.end.y -= endVector.y;
 }
 
 Star.prototype.draw = function() {
@@ -155,3 +197,40 @@ Vector.prototype.normalise = function() {
 		this.y /= m;
 	}
 };
+
+Vector.prototype.clone = function() {
+	return new Vector(this.x, this.y);
+};
+
+
+// UTILS
+
+function lineIntersect(x1,y1,x2,y2, x3,y3,x4,y4) {
+    var x=((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
+    var y=((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
+    if (isNaN(x)||isNaN(y)) {
+        return false;
+    } else {
+        if (x1>=x2) {
+            if (!(x2<=x&&x<=x1)) {return false;}
+        } else {
+            if (!(x1<=x&&x<=x2)) {return false;}
+        }
+        if (y1>=y2) {
+            if (!(y2<=y&&y<=y1)) {return false;}
+        } else {
+            if (!(y1<=y&&y<=y2)) {return false;}
+        }
+        if (x3>=x4) {
+            if (!(x4<=x&&x<=x3)) {return false;}
+        } else {
+            if (!(x3<=x&&x<=x4)) {return false;}
+        }
+        if (y3>=y4) {
+            if (!(y4<=y&&y<=y3)) {return false;}
+        } else {
+            if (!(y3<=y&&y<=y4)) {return false;}
+        }
+    }
+    return true;
+}

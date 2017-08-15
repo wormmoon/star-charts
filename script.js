a39580f7c5060b0b9bba8be5ed37347366016895
Emily Oliver <mleeoliver@gmail.com>
2017-08-15 14:56:39 +0100

    Add back in calcGaps

--- script.js
+++ script.js
@@ -162,9 +162,9 @@ function Line(start, end, startRadius, endRadius) {
 	this.end = end;
 	this.startRadius = startRadius;
 	this.endRadius = endRadius;
-	//this.calcGaps();
-	//this.noGapStart = start;
-	//this.noGapEnd = end;
+	this.calcGaps();
+	this.noGapStart = start;
+	this.noGapEnd = end;
 }
 
 Line.prototype.draw = function() {
@@ -176,20 +176,20 @@ Line.prototype.draw = function() {
 	ctx.stroke();
 };
 
-//Line.prototype.calcGaps = function() {
-//	var startVector = new Vector(this.end.x - this.start.x, this.end.y - this.start.y); //does this need to change?
-//	console.log(startVector);
-//	startVector.normalise();
-//	console.log(startVector);
-//	startVector.multiply(4 + this.startRadius); //How can I reference the size of that particular star from within here?
-//	var endVector = startVector.clone();
-//	endVector.normalise();
-//	endVector.multiply(4 + this.endRadius);
-//	this.start.x += startVector.x;
-//	this.start.y += startVector.y;
-//	this.end.x -= endVector.x;
-//	this.end.y -= endVector.y;
-//}
+Line.prototype.calcGaps = function() {
+	var startVector = new Vector(this.end.x - this.start.x, this.end.y - this.start.y); //does this need to change?
+	console.log(startVector);
+	startVector.normalise();
+	console.log(startVector);
+	startVector.multiply(4 + this.startRadius); //How can I reference the size of that particular star from within here?
+	var endVector = startVector.clone();
+	endVector.normalise();
+	endVector.multiply(4 + this.endRadius);
+	this.start.x += startVector.x;
+	this.start.y += startVector.y;
+	this.end.x -= endVector.x;
+	this.end.y -= endVector.y;
+}
 
 var first = true;
 
